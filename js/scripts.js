76081dd48d9e3c63490ec0c880a789e776cb5fa3
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-01-28 09:19:02 -0800

    add function for creating pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 function Order() {
   this.pizzas = {};
 }
+
+function Pizza (toppings,size) {
+  this.toppings = toppings;
+  this.size = size;
+
+}
+
+
+
+
+let myOrder = new Order(); 
+const pizza = new Pizza(["pineapple", "bacon"], 3);
+const pizza2 = new Pizza(["bacon"], 2);
+
+myOrder;
+pizza;
+pizza2;
